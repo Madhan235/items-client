f9f098cd678cf20e1125818bd68b688186ca854c
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-02-12 08:47:40 +0530

    link changed2

--- src/components/api.js
+++ src/components/api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const apiBaseUrl =  `https://items-dn8q.onrender.com`
 
-
+ 
 // `http://localhost:7000`
 
-// `https://items-dn8q.onrender.com`
+ 
 
 const api = axios.create({
     baseURL:apiBaseUrl,
